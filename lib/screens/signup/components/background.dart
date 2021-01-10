b8e2966dd3a6145f830c4ce3ada5253194d874e9
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2021-01-10 20:29:24 +0500

    designed signup screen

--- lib/screens/signup/components/background.dart
+++ lib/screens/signup/components/background.dart
@@ -21,16 +21,8 @@ class Background extends StatelessWidget {
             top: 0,
             left: 0,
             child: Image.asset(
-              "assets/images/signup_top.png",
-              width: size.width * 0.35,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_bottom.png",
-              width: size.width * 0.25,
+              "assets/images/image_12.png",
+              width: size.width,
             ),
           ),
           child,
--- lib/screens/signup/components/body.dart
+++ lib/screens/signup/components/body.dart
@@ -7,7 +7,7 @@ import 'package:greenify/components/already_have_an_account_acheck.dart';
 import 'package:greenify/components/rounded_button.dart';
 import 'package:greenify/components/rounded_input_field.dart';
 import 'package:greenify/components/rounded_password_field.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:greenify/constants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -20,13 +20,17 @@ class Body extends StatelessWidget {
           children: <Widget>[
             Text(
               "SIGNUP",
-              style: TextStyle(fontWeight: FontWeight.bold),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
             SizedBox(height: size.height * 0.03),
-            SvgPicture.asset(
-              "assets/icons/signup.svg",
-              height: size.height * 0.35,
+            Text(
+              "Greenify",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 70,
+                  color: kPrimaryColor),
             ),
+            SizedBox(height: size.height * 0.03),
             RoundedInputField(
               hintText: "Your Email",
               onChanged: (value) {},
